2b85ddf4edf06ab989478fc908a65ab0c684eb4c
Yauheni_Chupilin <Yauheni_Chupilin@epam.com>
2019-10-18 16:07:06 +0300

    update

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -31,7 +31,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PetClinicApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PetClinicApplication.class, args);
+        // SpringApplication.run(PetClinicApplication.class, args);
 		
 		// AtomicInteger aInt1 = new AtomicInteger(0);
         // AtomicInteger aInt2 = new AtomicInteger(0);
